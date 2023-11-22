10b52e8f66e2b8e7381fecf10a42e5d30375a95f
mokwathedeveloper <moffatmokwa12@gmail.com>
2023-11-22 08:15:09 +0300

    control flow how is temprature

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -8,7 +8,14 @@ def admin_login(username, password):
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
