41116537d6db5793e4233fde69d17a63422363c8
mokwathedeveloper <moffatmokwa12@gmail.com>
2023-11-22 08:17:49 +0300

    control flow calculator

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -18,8 +18,15 @@ def hows_the_weather(temperature):
         return "It's perfect out there!"
     pass
 
-def fizzbuzz(num):
-    # your code here
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
     pass
 
 def calculator(operation, num1, num2):
